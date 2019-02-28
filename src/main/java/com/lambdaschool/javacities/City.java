198fe4a32f73b935777a8fca362bd1250baf061d
charlesgodoy <charlesgodoy@live.com>
2019-02-28 18:29:54 -0500

    cities

---
+++ src/main/java/com/lambdaschool/javacities/City.java
@@ -0,0 +1,27 @@
+package com.lambdaschool.javacities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class City{
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private int medianPrice;
+    private int affordabilityIndex;
+
+    public City()
+    {
+        // default constructor
+    }
+
+        public City(String name, int medianPrice, int affordabilityIndex) {
+            this.name = name;
+            this.medianPrice = medianPrice;
+            this.affordabilityIndex = affordabilityIndex;
+        }
+}
